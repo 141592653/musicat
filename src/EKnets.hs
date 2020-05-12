7bdf9940b10fcc020d33cb9019b35d6bb8515b2c
Alice Rixte <alice.rixte@gmail.com>
2020-05-13 01:14:22 +0200

    thin and oneobject categories

--- src/EKnets.hs
+++ src/EKnets.hs
@@ -1,7 +1,34 @@
+{-# LANGUAGE GADTs #-}
+
 module EKnets
     ( 
 
     )
 where
 
-import Control.Category.Free
+import Control.Category
+import qualified Data.Function as Func (id,(.))
+
+
+newtype ThinCat a b = ThinCat {arrow :: a -> b}
+thinId :: ThinCat a a
+thinId = ThinCat {arrow = Func.id}
+thinCompose :: ThinCat b c -> ThinCat a b -> ThinCat a c
+thinCompose ThinCat {arrow = f} ThinCat {arrow = g} = 
+    ThinCat {arrow = f Func.. g}
+
+data OneObjCat a b where
+    EndoMorph :: a ~ b => (a -> b)-> OneObjCat a b
+
+oneObjId :: OneObjCat a a
+oneObjId = EndoMorph Func.id 
+oneObjCompose :: OneObjCat b c -> OneObjCat a b -> OneObjCat a c
+oneObjCompose (EndoMorph f) (EndoMorph g) = EndoMorph (f Func.. g)
+
+instance Category ThinCat where
+    id = thinId
+    (.) = thinCompose
+
+instance Category OneObjCat where 
+    id = oneObjId
+    (.) = oneObjCompose
