ea9e386b1f8d6d8b5a03fa5f5f1458bbaf99c5fa
Alice Rixte <alice.rixte@gmail.com>
2020-05-18 11:34:50 +0200

    Modified contraint categories

--- src/EKnets.hs
+++ src/EKnets.hs
@@ -1,4 +1,17 @@
-{-# LANGUAGE GADTs #-}
+-- {-# LANGUAGE GADTs                        #-}
+{-# LANGUAGE TypeFamilies                 #-}
+{-# LANGUAGE MultiParamTypeClasses        #-}
+-- {-# LANGUAGE TypeOperators                #-}
+-- {-# LANGUAGE ConstraintKinds              #-}
+-- {-# LANGUAGE FlexibleContexts             #-}
+-- {-# LANGUAGE RankNTypes                   #-}
+{-# LANGUAGE UnicodeSyntax                #-}
+-- {-# LANGUAGE AllowAmbiguousTypes          #-}
+
+-- {-# LANGUAGE CPP                          #-}
+-- #if __GLASGOW_HASKELL__ >= 800
+-- {-# LANGUAGE UndecidableSuperClasses      #-}
+-- #endif
 
 module EKnets
     ( 
@@ -7,4 +20,34 @@ module EKnets
 where
 
 import Control.Category.Constrained
-import qualified Data.Function as Func (id,(.))
+import Data.Constraint (Constraint)
+import qualified Data.Function as Func (id,const,(.))
+
+newtype ThinCat a b = ThinCat {arrow :: Maybe (a -> b)}
+thinId :: ThinCat a a
+thinId = ThinCat {arrow = Just Func.id}
+thinCompose :: ThinCat b c -> ThinCat a b -> ThinCat a c
+thinCompose ThinCat {arrow = Just f} ThinCat {arrow = Just g} = 
+    ThinCat {arrow = Just $ f Func.. g}
+
+instance Category ThinCat where
+    id = thinId
+    (.) = thinCompose
+
+type MonoidCat k x = ConstrainedCategory k ((~) x)
+type SimpleCat x = MonoidCat ThinCat x 
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
